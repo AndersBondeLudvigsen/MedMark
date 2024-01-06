8714a6c2c07fe44fedd53abf0e65d0b1b8445633
AndersBondeLudvigsen <anlu0001@stud.kea.dk>
2024-01-06 15:13:40 +0100

    1

--- src/main/java/CodeMessage/Main.java
+++ src/main/java/CodeMessage/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         CodeMessage codeMessage = new CodeMessage("Hello", 3);
 
-        // Vis resultaterne
         codeMessage.printCiphers();
     }
 }
